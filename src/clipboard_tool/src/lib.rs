28a305b7a01054329adec260d3185c2ec62c3769
478959472 <478959472@qq.com>
2023-05-15 15:43:40 +0800

    add the function of keeping the clipboard to a file,record the clipboard log

--- src/clipboard_tool/src/lib.rs
+++ src/clipboard_tool/src/lib.rs
@@ -1,6 +1,13 @@
 
 pub mod str_tool {
     use clipboard_win::{get_clipboard_string, set_clipboard_string};
+    use std::fs::OpenOptions;
+    use std::io::{self, Write};
+    use std::ops::Add;
+    use chrono::prelude::*;
+    use std::fs;
+    use std::path::Path;
+
     pub fn replace_clipboard() {
         // 读取剪切板内容
         let content = get_clipboard_string().unwrap();
@@ -10,5 +17,56 @@ pub mod str_tool {
 
         // 复制替换后的文本回剪切板
         set_clipboard_string(&replaced_str).unwrap();
+    }
+
+
+    pub fn log_clipboard(log_path: &str) -> io::Result<()> {
+        create_path(log_path);
+        let mut log_path = String::from(log_path);
+        if !log_path.ends_with("/") && !log_path.ends_with("\\"){
+            log_path.push('/')
+        }
+        // 读取剪切板内容
+        let  clipboard_content = get_clipboard_string().unwrap();
+
+        let mut content =format!("\r\n{}\r\n", Local::now().format("%Y-%m-%d %H:%M:%S") );
+        content.push_str(&clipboard_content);
+        // 获取当前日期
+        let today =Local::now();
+
+        let  filename = format!("{}.txt", today.format("%Y-%m-%d"));
+        let  filename = log_path.add(&filename);
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(&filename)?;
+        file.write_all(content.as_bytes())?;
+
+        Ok(())
+    }
+
+    fn create_path(file_path: &str){
+        // 检查文件路径是否存在
+        if Path::new(&file_path).exists() {
+            println!("文件路径已存在");
+        } else {
+            // 创建完整路径
+            if let Some(parent) = Path::new(&file_path).parent() {
+                if !parent.exists() {
+                    if let Err(err) = fs::create_dir_all(parent) {
+                        eprintln!("无法创建路径: {}", err);
+                        return;
+                    }
+                    println!("路径已创建");
+                }
+            }
+
+            // 创建文件
+            if let Err(err) = fs::File::create(&file_path) {
+                eprintln!("无法创建文件: {}", err);
+                return;
+            }
+            println!("文件已创建");
+        }
     }
 }
--- src/clipboard_tool/src/main.rs
+++ src/clipboard_tool/src/main.rs
@@ -1,6 +1,39 @@
 use clipboard_tool::str_tool;
+use std::env;
 
 // C:\Users\47895\Desktop\open_as_app.ps1
 fn main() {
-    str_tool::replace_clipboard();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: ./clipboard-tool --op-type log --log-path C:/Users/47895/iCloudDrive/Documents/clipboard_log");
+        println!("Usage: ./clipboard-tool --op-type format-win-path");
+        return;
+    }
+    let mut op_type = String::from("");
+    let mut log_path = String::from("");
+
+    for i in 1..args.len() {
+        match args[i].as_str() {
+            "--op-type" => {
+                op_type = args[i + 1].clone();
+            }
+            "--log-path" => {
+                log_path = args[i + 1].clone();
+            }
+            _ => {}
+        }
+    }
+    println!("-op-type: {}", op_type);
+    println!("-log-path: {}", log_path);
+    if "log".eq_ignore_ascii_case(&op_type) {
+        if args.len() < 5 {
+            println!("Usage: ./clipboard-tool --op-type log --log-path C:/Users/47895/iCloudDrive/Documents/clipboard_log");
+            return;
+        }
+        str_tool::log_clipboard(&log_path).expect("Failed to save clipboard");
+    } else if "format-win-path".eq_ignore_ascii_case(&op_type) {
+        str_tool::replace_clipboard();
+    } else {
+        println!("incorrect input parameters")
+    }
 }
